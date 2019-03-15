c9b0fd2183a97ce60119855b1c3edbce546e3a48
Emily Wang <46572364+emilyo9264@users.noreply.github.com>
2019-03-14 22:43:31 -0400

    Modified based on comments - Mar. 14

--- paper-collector/DeepLearningPaperScraper.py
+++ paper-collector/DeepLearningPaperScraper.py
@@ -2,74 +2,90 @@
 import arxiv
 import pandas as pd
 
-KEYWORD = '%28%22deep learning%22 OR %22neural network%22 OR %22GPU%22 OR %22graphics processing unit%22 OR %22reinforcement learning%22 OR %22perceptron%22%29'
+# Keywords include deep learning, neural network, GPU, graphics processing unit, 
+# reinforcement learning, OR perceptron
+keyword = '%28%22deep learning%22 OR %22neural network%22 \
+            OR %22GPU%22 OR %22graphics processing unit%22 \
+            OR %22reinforcement learning%22 OR %22perceptron%22%29'
 
-CATEGORY = '%28cat:cs.LG OR cat:stat.ML OR cat:cs.AI OR cat:cs.CV%29'
+# Searches within machine learning (stats or cs), artificial intelligence or computer vision
+category = '%28cat:cs.LG OR cat:stat.ML OR cat:cs.AI OR cat:cs.CV%29'
 
-QUERY = KEYWORD + " AND " + CATEGORY
+search_query = keyword + " AND " + category
 
-def firstTime():
+def article_id(article_dataframe):
+    temp_id = article_dataframe['id'].tolist()
+    unique_id = [item[-12:] for item in temp_id]
+    return unique_id
+
+def initial_articles():
     # Scraping articles for the first time
-    articles = arxiv.query(QUERY, max_results=9999, sort_by="lastUpdatedDate", sort_order="descending")
+    articles = arxiv.query(search_query, max_results=9999, 
+        sort_by="lastUpdatedDate", sort_order="descending")
 
-    articlesDF = pd.DataFrame.from_dict(articles)
-    orderedArticles = articlesDF.reindex(
+    articles_df = pd.DataFrame.from_dict(articles)
+    ordered_articles = articles_df.reindex(
         columns=['title', 'author', 'authors', 'id', 'arxiv_comment', 
                  'arxiv_primary_category', 'published', 'summary', 
                  'tags', 'updated'])
 
-    orderedArticles.to_json("DeepLearningArticles.json", orient='index')
+    ordered_articles.to_json("DeepLearningArticles.json", orient='index')
 
-def updateArticles():
+def update_articles():
     # Updating records
-    orderedArticles = pd.read_json("DeepLearningArticles.json", orient='index')
-    temp_id = orderedArticles['id'].tolist()
-    unique_id = [item[-12:-2] for item in temp_id]
-    version_no = [item[-1] for item in temp_id]
+    ordered_articles = pd.read_json("DeepLearningArticles.json", orient='index')
+    unique_id = article_id(ordered_articles)
+    version_no = [item[-1] for item in unique_id]
 
+    # Dictionary for existing article {unique id: version number}
     article_dict = {}
-    for i in range(len(temp_id)):
-        article_dict[unique_id[i]] = version_no[i]
+    for i in range(len(unique_id)):
+        article_dict[unique_id[i][:-2]] = version_no[i]
 
     # Getting new articles, change max_results
-    new_articles = arxiv.query(QUERY, max_results=100, sort_by="lastUpdatedDate", sort_order="descending")
-    new_articlesDF = pd.DataFrame.from_dict(new_articles)
-    ordered_new_artiDF = new_articlesDF.reindex(
+    new_articles = arxiv.query(search_query, max_results=100, 
+        sort_by="lastUpdatedDate", sort_order="descending")
+    new_articles_df = pd.DataFrame.from_dict(new_articles)
+    ordered_new_articles = new_articles_df.reindex(
         columns=['title', 'author', 'authors', 'id', 'arxiv_comment', 
                  'arxiv_primary_category', 'published', 'summary', 
                  'tags', 'updated'])
 
-    temp_id_2 = ordered_new_artiDF['id'].tolist()
+    unique_id_new = article_id(ordered_new_articles)
+    prefix = 'http://arxiv.org/abs/'
     counter = 0
 
-    for item in temp_id_2:
-        arti_key = item[-12:-2]
+    for item in unique_id_new:
+        article_key = item[:-2]
 
-        # Adding newly published articles
-        if arti_key not in article_dict:
-            article_dict[arti_key] = item[-1]
-            print("Updated the dictionary.")
-            orderedArticles = pd.concat(
-                [orderedArticles, ordered_new_artiDF[lambda ordered_new_artiDF: ordered_new_artiDF['id'] == item]], 
+        # Adding newly published articles (new articles appended at the front)
+        if article_key not in article_dict:
+            article_dict[article_key] = item[-1]
+            ordered_articles = pd.concat(
+                [ordered_new_articles[
+                    lambda ordered_new_articles: ordered_new_articles['id'] == prefix+item], 
+                    ordered_articles], 
                  axis=0, sort=False, ignore_index=True)
-            print("Added one new article.")
+            print("Added a new paper.")
             counter += 1
 
         # Updating old versions
         else:
-            if int(item[-1]) > int(article_dict[arti_key]):
-                article_index = orderedArticles.loc[orderedArticles['id'] == item].index[0]
-                orderedArticles.drop(labels=article_index, axis=0, inplace=True)
-                print("Deleted one old article.")
-                article_dict[arti_key] = item[-1]
-                print("Updated the dictionary.")
-                orderedArticles = pd.concat(
-                    [orderedArticles, ordered_new_artiDF[lambda ordered_new_artiDF: ordered_new_artiDF['id'] == item]], 
-                    axis=0, sort=False, ignore_index=True)
-                print("Added one new article.")
+            if int(item[-1]) > int(article_dict[article_key]):
+                old_article_id = prefix + item[:-1] + article_dict[article_key]
+                article_index = ordered_articles.index.get_loc(
+                    ordered_articles.index[ordered_articles['id'] == old_article_id][0])
+                ordered_articles.drop(labels=article_index, axis=0, inplace=True)
+                article_dict[article_key] = item[-1]
+                ordered_articles = pd.concat(
+                    [ordered_new_articles[
+                        lambda ordered_new_articles: ordered_new_articles['id'] == prefix+item], 
+                        ordered_articles], 
+                     axis=0, sort=False, ignore_index=True)
+                print("Updated a newer version.")
                 counter += 1
 
+    ordered_articles.to_json("DeepLearningArticles.json", orient='index')
     print("Update completed: {} updates made.".format(counter))
-    orderedArticles.to_json("DeepLearningArticles.json", orient='index')
 
-updateArticles()
+update_articles()
